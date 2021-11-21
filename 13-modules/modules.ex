b8e285c66ef87911dbc7c9b33780c4de3b5dc7d8
Cyytrus <antoniopaulo122@gmail.com>
2021-11-21 14:12:26 -0300

    add class number 13: modules

---
+++ 13-modules/modules.ex
@@ -0,0 +1,11 @@
+defmodule SaySomething do # defmodule sempre em CamelCase
+  def hello_world do  # def sempre em snake_case
+    "salve salve mundão"
+  end
+end
+
+defmodule MyModule.SaySomething do
+  def hello_world do
+    "salve salve mundãunnnn"
+  end
+end
